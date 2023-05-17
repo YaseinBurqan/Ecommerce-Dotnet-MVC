fcc2c8e8c8bd472c695596078b7e2872a58bd3ac
Yasein Burqan <Yasein Burqan@YASEINBURQAN>
2023-05-17 11:35:44 +0300

    Add project files.

---
+++ Ecommerce Dotnet MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce_Dotnet_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
